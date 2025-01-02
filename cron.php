01cc3d11e6a59c506f45b4bd49eafb0b6615c0ee
qugebert <44033795+qugebert@users.noreply.github.com>
2025-01-02 21:24:42 +0100

    Update cron.php

--- cron.php
+++ cron.php
@@ -11,6 +11,56 @@ $req_uri="https://api.telegram.org/bot".$tg_config['tgr_key']."/sendMessage?".ht
 file_get_contents($req_uri);
 }
 
+function checkNoteStatus($id) {
+    global $oauth2;
+    if (!$file=file_get_contents($oauth2['api_base_url']."notes/".$id.".json"))
+        return "404";
+    $note=json_decode($file,true);
+    print_r($note);
+
+}
+
+function commentNote($id,$text) {
+    global $oauth2;
+
+    $cl = curl_init();
+    curl_setopt($cl, CURLOPT_URL, $oauth2['api_base_url'] . "notes/".$id."/comment");
+    $headers = [
+        'Authorization: Bearer ' . $oauth2['bearer']
+    ];
+
+    curl_setopt($cl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($cl, CURLOPT_RETURNTRANSFER, true);    
+    curl_setopt($cl, CURLOPT_POST, true);
+    curl_setopt($cl, CURLOPT_POSTFIELDS, http_build_query([
+        "text" => $text, 
+    ]));
+    $result=curl_exec($cl);
+    curl_close($cl);
+    //return is_numeric($changeset_id) ? $changeset_id : false;
+
+}
+
+function reopenNote($id,$text) {
+    global $oauth2;
+
+    $cl = curl_init();
+    curl_setopt($cl, CURLOPT_URL, $oauth2['api_base_url'] . "notes/".$id."/reopen");
+    $headers = [
+        'Authorization: Bearer ' . $oauth2['bearer']
+    ];
+
+    curl_setopt($cl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($cl, CURLOPT_RETURNTRANSFER, true);    
+    curl_setopt($cl, CURLOPT_POST, true);
+    curl_setopt($cl, CURLOPT_POSTFIELDS, http_build_query([
+        "text" => $text, 
+    ]));
+    $result=curl_exec($cl);
+    curl_close($cl);
+    //return is_numeric($changeset_id) ? $changeset_id : false;
+
+}
 
 
 $config = json_decode(rtrim(file_get_contents("/run/secrets/mysqli_config_notes")), true);
