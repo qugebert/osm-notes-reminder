465d966fa222a9cd4a136e7f1b070c442f0e6e7b
Quirin Gebert <44033795+qugebert@users.noreply.github.com>
2025-07-25 10:53:25 +0200

    Update db_scheme.sql

--- db_scheme.sql
+++ db_scheme.sql
@@ -5,8 +5,6 @@ CREATE TABLE IF NOT EXISTS `reminder_bot` (
   `date` date NOT NULL,
   `user` int NOT NULL,
   `done` int NOT NULL DEFAULT '0',
-  `location_description` text,
-  `nominatim_json` json DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
