742c14b0d2892fba765bb5b967f8156f2e51565f
qugebert <44033795+qugebert@users.noreply.github.com>
2025-08-04 15:13:06 +0200

    move user-agent

--- includes/request_header.php
+++ includes/request_header.php
@@ -1,9 +1,9 @@
-
 <?php
+$user_agent=json_decode(file_get_contents("/run/secrets/oauth2_notes_reminder"),true)['User-Agent']??'NotesReminderBot/1.0';
 
     $options = [
         "http" => [
-            "header" => "User-Agent: OSMNotesReminder/1.0 (osm-bot@qugeb.de;https://osm.org/user/NotesReminderBot;https://github.com/qugebert/osm-notes-reminder/)\r\n"
+            "header" => "User-Agent: ".$user_agent."\r\n"
         ]
     ];
     $context = stream_context_create($options);
