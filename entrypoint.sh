c389a7522a963179328fbd565e741d16932f94f2
Quirin Gebert <44033795+qugebert@users.noreply.github.com>
2025-12-01 11:50:34 +0100

    Modify MySQL command to disable SSL mode
    Updated MySQL command to use SSL mode disabled for compatibility with existing Docker setup.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,8 @@ USER=$(echo "$CONFIG" | jq -r .user)
 PASS=$(echo "$CONFIG" | jq -r .pass)
 DB=$(echo "$CONFIG" | jq -r .db)
 
-mysql -h "$HOST" -u "$USER" -p"$PASS" "$DB" < /usr/src/app/db_scheme.sql
+# Vorsicht, ssl-mode=DISABLED ist nur im bestehenden Docker-Setup praktikabel wg. TLS/SSL error: self-signed certificate und ist eher eine Notlösung
+mysql --ssl-mode=DISABLED -h "$HOST" -u "$USER" -p"$PASS" "$DB" < /usr/src/app/db_scheme.sql
 
 echo "0 * * * * root /usr/local/bin/php /usr/src/app/cron.php" > /etc/cron.d/cron
 echo "0 6 1 * * root /usr/local/bin/php /usr/src/app/stats.php" >> /etc/cron.d/cron
